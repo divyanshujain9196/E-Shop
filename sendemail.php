09b41aa5f9e90b8fffac4cb3f41a7266f8d8f886
divyanshujain9196 <divyanshu.tca1959012@tmu.ac.in>
2021-09-18 15:07:37 +0530

    css updation

--- sendemail.php
+++ sendemail.php
@@ -1,39 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
 
-<?php 
-   
-   use PHPMailer\PHPMailer\PHPMailer; 
-   use PHPMailer\PHPMailer\Exception; 
-     
-   require 'src/Exception.php'; 
-   require 'src/PHPMailer.php'; 
-   require 'src/SMTP.php'; 
-     
-   //require 'vendor/autoload.php'; 
-     
-   $mail = new PHPMailer(true); 
-     
-   try { 
-            //    $mail->SMTPDebug = 2;                                                                                                    
-               $mail->isSMTP();                                                                                                                         
-               $mail->Host        = 'smtp.gmail.com;';                                                        
-               $mail->SMTPAuth = true;                                                                           
-               $mail->Username = 'djain01012002@gmail.com';                                     
-               $mail->Password = 'Divyanshu@20';                                                              
-               $mail->SMTPSecure = 'tls';                                                                         
-               $mail->Port         = 587; 
-     
-               $mail->setFrom("jdivyanshu66@gmail.com", "Divyanshu");             
-               $mail->addAddress('divyanshu.tca1959012@tmu.ac.in'); // where you want to send mail 
-               
-               
-               $mail->isHTML(true);                                                                                            
-               $mail->Subject = 'Enquiry about E-Store'; 
-               $mail->Body = "Name- ". $_POST["name"]. "<br>" . "Message -" .$_POST["message"]. "<br>" . "Email-id - ".$_POST["email"]; 
-               $mail->AltBody = 'Body in plain text for non-HTML mail clients'; 
-               $mail->send(); 
-               echo "Mail has been sent successfully!"; 
-   } catch (Exception $e) { 
-               echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}"; 
-   } 
-     
-   ?> 
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Contact| Life Style Store</title>
+        <link rel="shortcut icon" href="img/srtcticon.png" type="image/png">
+
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+        <script src="js/jquery.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </head>
+
+    <body>
+        <?php include 'includes/header.php'; ?>
+        <div style="" align=center >
+            <p> ThankYou for Giving Your Time! We Will Contact You Shortly.</p>
+        </div>
+</body>
+</html>
